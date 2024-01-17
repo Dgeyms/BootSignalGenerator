9e3922110627043ce5592eeba0c6e53358becec1
Dgeyms <Dgeyms@gmail.com>
2024-01-17 14:14:44 +0300

    fix(product): delete Class.forName
    Delete Class.forName("org.postgresql.Driver");

--- src/main/java/org/boottrading/repository/BootRepositoryImpl.java
+++ src/main/java/org/boottrading/repository/BootRepositoryImpl.java
@@ -1,7 +1,34 @@
 package org.boottrading.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.boottrading.util.PostgresJdbcConnection;
+
 public class BootRepositoryImpl implements BootRepository {
+
     public void saveBootDataBase(String nameBoot) {
+        try (Connection connection = DriverManager.getConnection(PostgresJdbcConnection.DB_URL,
+                PostgresJdbcConnection.USER, PostgresJdbcConnection.PASS)) {
+            Class.forName("org.postgresql.Driver");
+
+            String sql = "INSERT INTO boots (name_boot) VALUES (?)";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setString(1, nameBoot);
+                ps.executeUpdate();
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Не удалось загрузить драйвер JDBC PostgreSQL");
+            e.printStackTrace();
+            return;
+        } catch (SQLException e) {
+            System.out.println("Ошибка SQL!");
+            e.printStackTrace();
+        }
 
     }
 
---
+++ src/main/java/org/boottrading/util/PostgresJdbcConnection.java
@@ -0,0 +1,7 @@
+package org.boottrading.util;
+
+public class PostgresJdbcConnection {
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/my_database";
+    public static final String USER = "my_username";
+    public static final String PASS = "my_password";
+}
